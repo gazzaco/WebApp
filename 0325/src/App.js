9ff186ad466d245c84df8930b5d164d29d46ae18
gazzaco <kimprincess7777@gmail.com>
2026-03-28 16:32:24 +0900

    Add files via upload

---
+++ 0325/src/App.js
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import React from "react";
+import Clock from "./Clock";
+// Clock 컴포넌트 불러오기
+
+function App() {
+    return (
+        <div>
+            <Clock />
+        </div>
+    );
+}
+
+export default App;
---
+++ 0325/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+function tick() {
+    root.render(<App />);
+}
+setInterval(tick, 1000)
